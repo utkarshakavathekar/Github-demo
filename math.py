089707adf80d8dd0aa4e304ea74dd73fea3072b1
Utkarsha Kavathekar <47240934+utkarshakavathekar@users.noreply.github.com>
2021-07-02 14:20:55 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,21 @@
+# add implementation
+def add(x,y):
+    return x+y
+	
+#sub implementation	
+def subtract(x,y):
+    return x-y 				#done in bug456 branch
+	
+#mul implementation	
+def multiply(x,y):
+    return x*y				#done in master branch
+	
+#div implementation	
+def divide(x,y):
+    if y==0:					#Done by resolving conflicts
+		return DIVIDE_BY_0_ERROR
+	else:
+		return x/y
+
+def square(x):
+	pass
