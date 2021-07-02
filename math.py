71159e380731a94ce64836adf8e1e9b48ad3eb81
Utkarsha Kavathekar <kavathekarsayali23@gmail.com>
2021-07-02 14:53:28 +0530

    Implemented mul=x*y and returned mul

--- math.py
+++ math.py
@@ -4,8 +4,9 @@ def add(x,y):
 def subtract(x,y):
     return x-y
 
-def multiply(x,y):
-    return x*y				
+def multiply(x,y):	
+    mul = x*y		#in bug456
+	return mul
 
 def divide(x,y):
     if y==0:					#Done by resolving conflicts
